e04ba25fb42ad1076e29cabcb57d956534ad3cbc
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-05-28 19:51:09 -0400

    Added jQuery time-block

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 //jQuery
 $(document).ready(function(){
-    const currentTime = moment(new Date()).format('k');
+    const currentDay = moment(new Date());
+
+    $('#currentDay').append(`${currentDay.format('dddd, LL')}`)
 
     for (let time=9; time<=17; time++){
-        const myColor = (currentTime == time)? 'present': (currentTime < time)? 'future' : 'past';
+        const myColor = (currentDay.format('k') == time)? 'present': (currentDay.format('k') < time)? 'future' : 'past';
 
         $('.container').append(`
         <div class='row time-block'>
             <div class='col-2 col-md-1 hour'>
                 ${moment(time.toString(),"LT").format('h A')}
             </div>
-            <div class='col ${myColor}' style='padding:0px' id='col${time}'></div>
+            <div class='col ${myColor}' style='padding:0px' id='timeCol${time}'></div>
+            <div class='col-2 col-md-1' style='padding:0px' id='buttonCol${time}'></div>
         </div>`);
 
-        $(`#col${time}`).append(`<textarea style='width:100%; height:100%;' id="textArea${time}"></textarea>`);
+        $(`#timeCol${time}`).append(`<textarea style='width:100%; height:100%;' id="textArea${time}"></textarea>`);
+        $(`#buttonCol${time}`).append(`<button class="saveBtn" style='width:100%; height:100%;' if="saveBtn${time}"><i>Save</i></button>`);
     }
 })
